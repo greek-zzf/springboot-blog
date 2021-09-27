c060705070198f17fa7616482727f17335db0a5d
Zhouzf <aaaa159557190@gmail.com>
2021-09-27 15:31:26 +0800

    add global package and add JwtAuthenticationfilter

--- src/main/java/com/springboot/blog/config/WebSecurityConfig.java
+++ src/main/java/com/springboot/blog/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.springboot.blog.config;
 
+import com.springboot.blog.filter.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,9 +11,12 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.inject.Inject;
 import javax.inject.Named;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 /**
  * @author Zhouzf
@@ -26,10 +30,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Inject
     private UserDetailsService userDetailsService;
 
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.authorizeRequests().antMatchers("/auth/**").permitAll();
+        http.csrf().disable()
+                .sessionManagement().sessionCreationPolicy(STATELESS)
+
+                .and()
+                .authorizeRequests().antMatchers("/auth/**").permitAll()
+                .anyRequest().authenticated()
+
+                .and()
+                .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+
     }
 
 
@@ -46,6 +59,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter();
     }
 
 }
